25e6d2f24e794227f117ea6e75ce0a7ef0fdbf65
Shubhojit Bagchi <75479624+Jit-EASE@users.noreply.github.com>
2025-10-26 01:30:36 +0100

    Update title in Dairy Decision Tool header

--- Dairy_Decision_Tool.py
+++ Dairy_Decision_Tool.py
@@ -22,7 +22,7 @@ st.set_page_config(page_title="Irish Dairy — Executive Intelligent Tool", layo
 st.markdown(
     """
 <div style="text-align:center; background:#e8f0fe; padding:1rem; border-radius:12px;">
-  <h2>Self Learning Dairy Processing Decision Tool (Autonomous) — Ireland </em></h2>
+  <h2>Autonomous Dairy Processing Decision Tool — Ireland </em></h2>
   <p>Self Aware Adaptive Multimodal System (SAAMS) </p>
 </div>
 """,
